83dadba790a78b20416122e8356ab7c670021dd4
Biruk <biruklemma24@gmail.com>
2026-09-24 20:13:32 +0300

    initialize barber booking project

---
+++ barber-booking/src/main/java/com/barberbooking/BarberBookingApplication.java
@@ -0,0 +1,14 @@
+package com.barberbooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarberBookingApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(BarberBookingApplication.class, args);
+	}
+
+}
---
+++ barber-booking/src/test/java/com/barberbooking/BarberBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.barberbooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BarberBookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
